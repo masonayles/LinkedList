07c1a3d290dfda708364636eb7e1eafeebaad7c3
Mason Ayles <mayles21@georgefox.edu>
2023-10-24 22:40:17 -0700

    updates 10:40 pm 10/24/23

--- LinkedList.java
+++ LinkedList.java
@@ -9,20 +9,20 @@ import java.util.ConcurrentModificationException;
  */
 public class LinkedList<E> implements Iterable<E>
 {
-   private LinkedListNode head;
-   private LinkedListNode tail;
-   private int size;
-   private long modcount;
+   private LinkedListNode _head;
+   private LinkedListNode _tail;
+   private int _size;
+   private long _modcount;
 
    /**
     * Constructs an empty list
     */
    public LinkedList()
    {
-      head = null;
-      tail = null;
-      size = 0;
-      modcount = 0;
+      _head = null;
+      _tail = null;
+      _size = 0;
+      _modcount = 0;
    }
 
    /**
@@ -35,19 +35,19 @@ public class LinkedList<E> implements Iterable<E>
     */
    public void add(int index, E element)
    {
-      if (index < 0 || index > size)
+      if (index < 0 || index > _size)
       {
          throw new IndexOutOfBoundsException("Invalid Index");
       }
       // Appending to the end of the list if the condition is true.
-      if (index == size)
+      if (index == _size)
       {
          addLast(element);
       }
       // If the condition above is false we need to insert the element at a specified position.
       else
       {
-         LinkedListNode currentNode = head;
+         LinkedListNode currentNode = _head;
          // linear searching the list to find the node at specified index.
          for (int i = 0; i < index; i++)
          {
@@ -63,25 +63,25 @@ public class LinkedList<E> implements Iterable<E>
          }
          else
          {
-            head = newNode;
+            _head = newNode;
          }
          // Updating the current node to point to the new node.
          currentNode.setPrevious(newNode);
-         size++;
-         modcount++;
+         _size++;
+         _modcount++;
       }
    }
 
    /**
     * This method appends the specified element to the end of this list.
-    * @param element element is the specified element that is to be appeneded to this list.
+    * @param element element is the specified element that is to be appended to this list.
     * @return returns true
     */
    public boolean add(E element)
    {
       addLast(element);
-      modcount++;
-      size++;
+      _modcount++;
+      _size++;
       return true;
    }
 
@@ -90,10 +90,18 @@ public class LinkedList<E> implements Iterable<E>
     */
    public void clear()
    {
-      head = null;
-      tail = null;
-      size = 0;
-      modcount ++; // TODO: do I increment modcount in clear?
+      LinkedListNode currentNode = _head;
+
+      while (currentNode != null) {
+         LinkedListNode nextNode = currentNode.getNext();
+         currentNode.setNext(null);
+         currentNode.setPrevious(null);
+         currentNode = nextNode;
+      }
+      _head = null;
+      _tail = null;
+      _size = 0;
+      _modcount ++; // TODO: do I increment modcount in clear?
    }
 
    /**
@@ -101,20 +109,23 @@ public class LinkedList<E> implements Iterable<E>
     * @param index index is the specified index of the element to return.
     * @return returns the element at the specified position in this list.
     */
-   public E get(int index)
-   {
-      if (index < 0 || index >= size)
-      {
+   public E get(int index) {
+      if (index < 0 || index >= _size) {
          throw new IndexOutOfBoundsException("Index out of bounds");
       }
-      // Reference to the head which is used to linear search through the list.
-      LinkedListNode currentNode = head;
 
-      // Linear searching through the list to find the node at specified location.
-      for (int i = 0; i < index; i++)
-      {
+      if (index == _size - 1) {
+         // Special case: Get the last element (index == _size - 1)
+         return _tail.getValue();
+      }
+
+      LinkedListNode currentNode = _head;
+
+      // Linear searching through the list to find the node at the specified index.
+      for (int i = 0; i < index; i++) {
          currentNode = currentNode.getNext();
       }
+
       return currentNode.getValue();
    }
 
@@ -127,7 +138,18 @@ public class LinkedList<E> implements Iterable<E>
     */
    public int indexOf(E element)
    {
+      int index = 0;
+      LinkedListNode current = _head;
 
+      //Linear searching through the whole list
+      while (current != null) {
+         if ((element == null && current.getValue() == null) || (element != null && element.equals(current.getValue()))) {
+            return index;
+         }
+         current = current.getNext();
+         index++;
+      }
+      return -1;
    }
 
    /**
@@ -136,7 +158,7 @@ public class LinkedList<E> implements Iterable<E>
     */
    public boolean isEmpty()
    {
-      return size == 0;
+      return _size == 0;
    }
 
    /**
@@ -149,38 +171,75 @@ public class LinkedList<E> implements Iterable<E>
     */
    public E remove(int index)
    {
-
-
-      modcount++;
-      size--;
-
-   }
-
-   /**
-    * Replaces the element at the specified position in this list
-    * with the specified element.
-    * @param index index is the index of the element to replace.
-    * @param element element to be stored at the specified position.
-    * @return returning the element previously at the specified position.
-    */
-   public E set(int index, E element)
-   {
-      if (index > 0 || index >= size)
+      if (index < 0 || index >= _size)
       {
          throw new IndexOutOfBoundsException("Index out of bounds");
       }
-      LinkedListNode currentNode = head;
-      // Linear searching through the list to find the node at the specified index.
-      for (int i = 0; i < index; i++)
+      // If index = 0 call my remove first method and same for last
+      if (index == 0)
       {
+         return removeFirst();
+      }
+      else if (index == _size - 1)
+      {
+         return removeLast();
+      }
+      else
+      {
+         // If the index is not found in the first or last this is how I
+         // find the node at the specified index.
+         LinkedListNode currentNode = _head;
+         for(int i = 0; i < index; i++)
+         {
+            currentNode = currentNode.getNext();
+         }
+         // Updates the reverence for previous and next.
+         LinkedListNode previous = currentNode.getPrevious();
+         LinkedListNode next = currentNode.getNext();
+         previous.setNext(next);
+         next.setPrevious(previous);
+         _size--;
+         _modcount++;
+         // Returning the element that was removed.
+         return currentNode.getValue();
+      }
+   }
+
+   public E set(int index, E element) {
+      if (index < 0 || index >= _size) {
+         throw new IndexOutOfBoundsException("Index out of bounds");
+      }
+
+      if (index == _size - 1) {
+         // Special case: Set the last element (index == _size - 1)
+         if (_size == 1) {
+            // If there's only one element in the list, update both head and tail.
+            E removedElement = _head.getValue();
+            _head.setValue(element);
+            _tail.setValue(element);
+            return removedElement;
+         } else {
+            // If there's more than one element, update the tail.
+            E removedElement = _tail.getValue();
+            _tail.setValue(element);
+            return removedElement;
+         }
+      }
+
+      LinkedListNode currentNode = _head;
+
+      // Linear searching through the list to find the node at the specified index.
+      for (int i = 0; i < index; i++) {
          currentNode = currentNode.getNext();
       }
+
       // Storing the value to be replaced
       E removedElement = currentNode.getValue();
-      // Updating the value of the node at the specified index with new element.
+      // Updating the value of the node at the specified index with the new element.
       currentNode.setValue(element);
       return removedElement;
    }
+
 
    /**
     * Returns the number of elements in this list.
@@ -188,7 +247,7 @@ public class LinkedList<E> implements Iterable<E>
     */
    public int size()
    {
-      return size;
+      return _size;
    }
 
    /**
@@ -221,20 +280,20 @@ public class LinkedList<E> implements Iterable<E>
       {
          // If the list is empty, create a new node with the provided element
          // and make it both the head and the tail of the list.
-         head = new LinkedListNode(element);
-         tail = head;
+         _head = new LinkedListNode(element);
+         _tail = _head;
       }
       else
       {
          // If the list is not empty, create a new node with the provided element,
          // and set its "next" reference to the current head.
-         LinkedListNode newNode = new LinkedListNode(element, null, head);
+         LinkedListNode newNode = new LinkedListNode(element, null, _head);
          // Update the previous reference of the current head to point to the new node.
-         head.setPrevious(newNode);
+         _head.setPrevious(newNode);
          // Update the head of the list to be the new node.
-         head = newNode;
+         _head = newNode;
       }
-      size++;
+      _size++;
    }
 
    /**
@@ -246,16 +305,16 @@ public class LinkedList<E> implements Iterable<E>
    {
       if (isEmpty())
       {
-         head = new LinkedListNode(element);
-         tail = head;
+         _head = new LinkedListNode(element);
+         _tail = _head;
       }
       else
       {
-         LinkedListNode newNode = new LinkedListNode(element, tail, null);
-         tail.setNext(newNode);
-         tail = newNode;
+         LinkedListNode newNode = new LinkedListNode(element, _tail, null);
+         _tail.setNext(newNode);
+         _tail = newNode;
       }
-      size++;
+      _size++;
    }
 
    /**
@@ -264,27 +323,30 @@ public class LinkedList<E> implements Iterable<E>
     */
    private E removeFirst()
    {
-      if (head == null)
+      if (_head == null)
       {
          throw new NoSuchElementException("List is empty");
       }
       // Getting the value of the first element.
-      E elementRemoved = head.getValue();
+      E elementRemoved = _head.getValue();
 
-      if (head == tail)
+      if (_head == _tail)
       {
          // If theirs only one element, set both the head and tail
          // to null making the lit empty
-         head = null;
-         tail = null;
+         _head = null;
+         _tail = null;
       }
       // Updating the head to be the next element.
       else
       {
-         head = head.getNext();
-         head.setPrevious(null);
+         _head = _head.getNext();
+         if (_head != null)
+         {
+            _head.setPrevious(null);
+         }
       }
-      size--;
+      _size--;
       return elementRemoved;
    }
 
@@ -294,23 +356,23 @@ public class LinkedList<E> implements Iterable<E>
     */
    private E removeLast()
    {
-      if (tail == null)
+      if (_tail == null)
       {
          throw new NoSuchElementException("List is empty");
       }
-      E elementRemoved = tail.getValue();
+      E elementRemoved = _tail.getValue();
 
-      if (head == tail)
+      if (_head == _tail)
       {
-         head = null;
-         tail = null;
+         _head = null;
+         _tail = null;
       }
       else
       {
-         tail = tail.getPrevious();
-         tail.setNext(null);
+         _tail = _tail.getPrevious();
+         _tail.setNext(null);
       }
-      size--;
+      _size--;
       return elementRemoved;
    }
 
@@ -393,7 +455,7 @@ public class LinkedList<E> implements Iterable<E>
 
       /**
        * setPrevious is assigning _previous to prev.
-       * @param prev
+       * @param prev Prev is the setting _previous to prev.
        */
       public void setPrevious(LinkedListNode prev)
       {
@@ -401,8 +463,8 @@ public class LinkedList<E> implements Iterable<E>
       }
 
       /**
-       *
-       * @param next
+       * setNext method is assigning the next LinkedListNode to next.
+       * @param next _next is being set to next
        */
       public void setNext(LinkedListNode next)
       {
@@ -411,7 +473,9 @@ public class LinkedList<E> implements Iterable<E>
    }
 
    /**
-    *
+    * Inner class that implements Iterator interface, and it uses the LinkedListNode
+    * and new variable cursor to keep track of where the iterator is through hasNext
+    * method.
     */
    public class LinkedListIterator implements Iterator<E>
    {
@@ -426,18 +490,27 @@ public class LinkedList<E> implements Iterable<E>
       public LinkedListIterator(boolean reverse)
       {
          this._reverse = reverse;
-
          // Get the current mod count.
-         this._expectedModCount = modcount;
+         this._expectedModCount = _modcount;
+
+         if (reverse)
+         {
+            _cursor = _tail;
+         }
+         else
+         {
+            _cursor = _head;
+         }
       }
 
       /**
-       *
-       * @return
+       * hasNext method checks for a concurrent modification exception throughout the iteration.
+       * and throws an error if the list has been modified during the iteraton.
+       * @return Returning _cursor != null.
        */
       public boolean hasNext()
       {
-         if (_expectedModCount != modcount)
+         if (_expectedModCount != _modcount)
          {
          throw new ConcurrentModificationException("List has been modified");
          }
@@ -445,12 +518,13 @@ public class LinkedList<E> implements Iterable<E>
       }
 
       /**
-       *
+       * next() is a method that checks for concurrent modification exceptions
+       * and throws an error if the list was modified through the iteration.
        * @return
        */
       public E next()
       {
-         if (_expectedModCount != modcount)
+         if (_expectedModCount != _modcount)
          {
             throw new ConcurrentModificationException("List has been modified");
          }
@@ -458,8 +532,17 @@ public class LinkedList<E> implements Iterable<E>
          {
             throw new NoSuchElementException();
          }
+         E element = _cursor.getValue();
 
-
+         if (_reverse)
+         {
+            _cursor = _cursor.getPrevious();
+         }
+         else
+         {
+            _cursor = _cursor.getNext();
+         }
+         return element;
       }
    }
 }
